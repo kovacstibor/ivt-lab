054bfaca1439c88e32ea0117c97f9ceca2021c99
kovacstibor <kovacs.tibor@outlook.hu>
2017-04-27 23:56:57 -0700

    Made GT4500 testable (using DI) and modified testes to use Mocito

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -1,40 +1,65 @@
 package hu.bme.mit.spaceship;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
-
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
 
 public class GT4500Test {
 
   private GT4500 ship;
 
+  private TorpedoStore primary;
+  private TorpedoStore secondary;
+
   @Before
   public void init(){
-    this.ship = new GT4500();
+    primary = Mockito.mock(TorpedoStore.class);
+    secondary = Mockito.mock(TorpedoStore.class);
+
+    this.ship = new GT4500(primary, secondary);
   }
 
   @Test
   public void fireTorpedos_Single_Success(){
     // Arrange
+    Mockito.when(primary.isEmpty()).thenReturn(false);
+    Mockito.when(primary.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedos(FiringMode.SINGLE);
 
     // Assert
     assertEquals(true, result);
+
+    // Verify
+    Mockito.verify(primary, times(1)).isEmpty();
+    Mockito.verify(primary, times(1)).fire(1);
   }
 
   @Test
   public void fireTorpedos_All_Success(){
     // Arrange
+    Mockito.when(primary.isEmpty()).thenReturn(false);
+    Mockito.when(primary.fire(1)).thenReturn(true);
+
+    Mockito.when(secondary.isEmpty()).thenReturn(false);
+    Mockito.when(secondary.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedos(FiringMode.ALL);
 
     // Assert
     assertEquals(true, result);
+
+    // Verify
+    Mockito.verify(primary, times(1)).isEmpty();
+    Mockito.verify(secondary, times(1)).isEmpty();
+
+    Mockito.verify(primary, times(1)).fire(1);
+    Mockito.verify(secondary, times(1)).fire(1);
   }
 
 }
